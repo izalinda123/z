c51ecafe4602ec5d331ea5f8f3c22ce90e6113dc
izalinda123 <149380940+izalinda123@users.noreply.github.com>
2023-11-10 08:27:31 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,17 @@
+slide1();
+function slide1()
+{
+    
+function slide1(){
+    document.getElementById('imag').src="yamaha";
+    setTimeout("slide2()", 1000).href="link1.html"
+    }
+function slide2(){
+    document.getElementById('imag').src="honda.jpg";
+    setTimeout("slide3()", 1000).href="link2.html"
+    }
+function slide3(){
+    document.getElementById('imag').src="honda.jpg";
+    setTimeout("slide1()", 1000).href="link3.html"}
+
+}
